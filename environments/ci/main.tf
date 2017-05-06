1f5205a1b02154c4d25277f55a23b818b5a052f2
Lance R. Vick <lance@lrvick.net>
2017-05-06 02:42:34 -0700

    drop ECS for in favor of cloud-config for droneci

--- environments/ci/main.tf
+++ environments/ci/main.tf
@@ -21,27 +21,33 @@ module "vpc" {
     enable_dns_support = "true"
 }
 
-module "droneci_balancer" {
+module "drone_balancer" {
     source = "../../modules/route53_balancer"
     domain = "ci.lrvick.net"
-    name = "${module.droneci_cluster.name}"
-    asg = "${module.droneci_cluster.asg_id}"
-    launch_topic = "${module.droneci_cluster.launch_topic}"
-    terminate_topic = "${module.droneci_cluster.terminate_topic}"
+    name = "droneci-balancer"
+    asg = "${module.drone_asg.id}"
+    launch_topic = "${module.drone_asg.launch_topic}"
+    terminate_topic = "${module.drone_asg.terminate_topic}"
 }
 
-module "droneci_cluster" {
-    source = "../../modules/ecs_cluster"
-    name = "droneci"
+module "drone_asg" {
+    source = "../../modules/coreos_asg"
+    name = "drone"
     key_name = "admin"
     subnets = "${module.vpc.public_subnets}"
     vpc_id = "${module.vpc.vpc_id}"
+    kms_key_id = "5a5fecb6-77d2-42e9-82f1-884dbee74c76"
+    cloud_config = "${data.template_file.cloud_config.rendered}"
 }
 
-module "droneci_service" {
-    source = "../../modules/ecs_service"
-    name = "droneci"
-    cluster_name = "${module.droneci_cluster.name}"
-    task_file = "${path.root}/tasks/droneci.json"
-    kms_key_id = "5a5fecb6-77d2-42e9-82f1-884dbee74c76"
+data "template_file" "cloud_config" {
+    template = "${file("${path.module}/drone-cloud-config.yml")}"
+    vars {
+        aws_region = "us-west-2"
+        kms_drone_github_client = "AQECAHjxFVv28s35RRShRB2bYo11qtmAuBRaqhGRrhmkdMTRYgAAAHIwcAYJKoZIhvcNAQcGoGMwYQIBADBcBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDCxWZFvY5VddfEyz6QIBEIAv5O5Vg6W6tmQaNYW3jrbX71PMf6ZrzzwvtBdakQLGk0VfDpqCDvphWzEXMb9vd2o="
+        kms_drone_github_secret = "AQECAHjxFVv28s35RRShRB2bYo11qtmAuBRaqhGRrhmkdMTRYgAAAIcwgYQGCSqGSIb3DQEHBqB3MHUCAQAwcAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxq8f21DDHmNkXpG84CARCAQ5w5DUqkRTM65cS+Tb2MQZ5/gpLE8XCZ+lLUc2r8aoO11VoX+kIj1yOs5FRsXcWiQkTre5gA7fUtfG6r7StBbKIxLHM="
+        kms_drone_secret = "AQECAHjxFVv28s35RRShRB2bYo11qtmAuBRaqhGRrhmkdMTRYgAAAKQwgaEGCSqGSIb3DQEHBqCBkzCBkAIBADCBigYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAwIb1iU4qYmJMUY3yUCARCAXQHkjL1W7Vikyi4VX3R4h/pm+lX14gQBzSSuX7pxbxlwMxt1ZdpeisjbbpUC6ccK/o3IAXKgj9D7DY0Yt+lQ8ph4/j8cjOfUlQI/POgrEqdV+P0LZdppE0UwSWJsnA=="
+        drone_orgs = ""
+        efs_id = "${module.drone_asg.efs_id}"
+    }
 }

f1f7bb58bdb7d9b5bad87808432b8b9904a87234
Lance R. Vick <lance@lrvick.net>
2017-05-08 02:06:18 -0700

    nginx proxy with letsencrypt

--- environments/ci/main.tf
+++ environments/ci/main.tf
@@ -41,13 +41,16 @@ module "drone_asg" {
 }
 
 data "template_file" "cloud_config" {
-    template = "${file("${path.module}/drone-cloud-config.yml")}"
+    template = "${file("${path.module}/files/drone-cloud-config.yml")}"
     vars {
         aws_region = "us-west-2"
         kms_drone_github_client = "AQECAHjxFVv28s35RRShRB2bYo11qtmAuBRaqhGRrhmkdMTRYgAAAHIwcAYJKoZIhvcNAQcGoGMwYQIBADBcBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDCxWZFvY5VddfEyz6QIBEIAv5O5Vg6W6tmQaNYW3jrbX71PMf6ZrzzwvtBdakQLGk0VfDpqCDvphWzEXMb9vd2o="
         kms_drone_github_secret = "AQECAHjxFVv28s35RRShRB2bYo11qtmAuBRaqhGRrhmkdMTRYgAAAIcwgYQGCSqGSIb3DQEHBqB3MHUCAQAwcAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxq8f21DDHmNkXpG84CARCAQ5w5DUqkRTM65cS+Tb2MQZ5/gpLE8XCZ+lLUc2r8aoO11VoX+kIj1yOs5FRsXcWiQkTre5gA7fUtfG6r7StBbKIxLHM="
         kms_drone_secret = "AQECAHjxFVv28s35RRShRB2bYo11qtmAuBRaqhGRrhmkdMTRYgAAAKQwgaEGCSqGSIb3DQEHBqCBkzCBkAIBADCBigYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAwIb1iU4qYmJMUY3yUCARCAXQHkjL1W7Vikyi4VX3R4h/pm+lX14gQBzSSuX7pxbxlwMxt1ZdpeisjbbpUC6ccK/o3IAXKgj9D7DY0Yt+lQ8ph4/j8cjOfUlQI/POgrEqdV+P0LZdppE0UwSWJsnA=="
         drone_orgs = ""
+        decrypt_script = "${file("${path.module}/files/decrypt.sh")}"
         efs_id = "${module.drone_asg.efs_id}"
+        domain = "ci.lrvick.net"
+        letsencrypt_email = "lance@lrvick.net"
     }
 }

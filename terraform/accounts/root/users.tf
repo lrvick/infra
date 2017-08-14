6043487045aab6d42da32af56392d1266c472e04
Lance R. Vick <lance@lrvick.net>
2017-08-14 15:12:25 -0700

    output gpg encrypted aws password for users

--- terraform/accounts/root/users.tf
+++ terraform/accounts/root/users.tf
@@ -2,6 +2,13 @@ resource "aws_iam_user" "lrvick" {
   name = "lrvick"
 }
 
+resource "aws_iam_user_login_profile" "lrvick" {
+  user = "${aws_iam_user.lrvick.name}",
+  pgp_key = "${file("../../files/lrvick.asc")}"
+  password_reset_required = true
+  password_length = 50
+}
+
 resource "aws_iam_user_ssh_key" "lrvick" {
   username = "${aws_iam_user.lrvick.name}"
   encoding = "PEM"
@@ -11,4 +18,8 @@ resource "aws_iam_user_ssh_key" "lrvick" {
 resource "aws_key_pair" "lrvick" {
   key_name = "lrvick"
   public_key = "${file("../../files/lrvick.pub")}"
+}
+
+output "lrvick_password" {
+  value = "${aws_iam_user_login_profile.lrvick.encrypted_password}"
 }

a88fccb616229de8bba1c3484107aa2c5f9645e7
Lance R. Vick <lance@lrvick.net>
2017-08-20 06:54:59 -0700

    refactor dns, add subnet

---
+++ terraform/accounts/production/dns.tf
@@ -0,0 +1,41 @@
+resource "aws_route53_zone" "personal" {
+    name = "lrvick.net"
+}
+
+resource "aws_route53_record" "wildcard" {
+    zone_id = "${aws_route53_zone.personal.zone_id}"
+    name = "*.lrvick.net"
+    type = "A"
+    ttl  = "300"
+    records = ["104.207.150.22"]
+}
+
+resource "aws_route53_record" "base" {
+    zone_id = "${aws_route53_zone.personal.zone_id}"
+    name = "${aws_route53_zone.personal.name}"
+    type = "A"
+    ttl = "300"
+    records = ["104.207.150.22"]
+}
+
+resource "aws_route53_record" "keybase" {
+    zone_id = "${aws_route53_zone.personal.zone_id}"
+    name = "lrvick.net"
+    type = "TXT"
+    ttl = "300"
+    records = ["keybase-site-verification=LyzhpqOMwzciC47VCWjWGszpCDzzvDw4fG7OK0P3zws"]
+}
+
+resource "aws_route53_record" "mx" {
+    zone_id = "${aws_route53_zone.personal.zone_id}"
+    name = "${aws_route53_zone.personal.name}"
+    type = "MX"
+    ttl = "300"
+    records = [
+        "1 aspmx.l.google.com",
+        "5 alt1.aspmx.l.google.com",
+        "5 alt2.aspmx.l.google.com",
+        "10 alt3.aspmx.l.google.com",
+        "10 alt4.aspmx.l.google.com"
+    ]
+}
--- terraform/accounts/production/main.tf
+++ terraform/accounts/production/main.tf
@@ -15,63 +15,18 @@ resource "aws_iam_account_alias" "alias" {
     account_alias = "lrvick-production"
 }
 
-resource "aws_route53_zone" "personal" {
-    name = "lrvick.net"
-    tags {
-        Environment = "production"
-    }
-}
-
-resource "aws_route53_record" "wildcard" {
-    zone_id = "${aws_route53_zone.personal.zone_id}"
-    name = "*.lrvick.net"
-    type = "A"
-    ttl  = "300"
-    records = ["104.207.150.22"]
-}
-
-resource "aws_route53_record" "base" {
-    zone_id = "${aws_route53_zone.personal.zone_id}"
-    name = "${aws_route53_zone.personal.name}"
-    type = "A"
-    ttl = "300"
-    records = ["104.207.150.22"]
-}
-
-resource "aws_route53_record" "keybase" {
-    zone_id = "${aws_route53_zone.personal.zone_id}"
-    name = "lrvick.net"
-    type = "TXT"
-    ttl = "300"
-    records = ["keybase-site-verification=LyzhpqOMwzciC47VCWjWGszpCDzzvDw4fG7OK0P3zws"]
-}
-
-resource "aws_route53_record" "mx" {
-    zone_id = "${aws_route53_zone.personal.zone_id}"
-    name = "${aws_route53_zone.personal.name}"
-    type = "MX"
-    ttl = "300"
-    records = [
-        "1 aspmx.l.google.com",
-        "5 alt1.aspmx.l.google.com",
-        "5 alt2.aspmx.l.google.com",
-        "10 alt3.aspmx.l.google.com",
-        "10 alt4.aspmx.l.google.com"
-    ]
-}
-
 module "vpc" {
     source = "../../modules/vpc"
     name = "production"
     cidr = "10.0.0.0/16"
-    public_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
-    azs = ["us-west-2a", "us-west-2b"]
+    public_subnets = ["10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24"]
+    azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
     enable_dns_hostnames = "true"
     enable_dns_support = "true"
 }
 
-resource "aws_key_pair" "admin" {
-    key_name = "admin"
+resource "aws_key_pair" "lrvick" {
+    key_name = "lrvick"
     public_key = "${file("../../files/lrvick_ssh.pub")}"
 }
 
